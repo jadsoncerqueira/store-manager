62905421846dfe146334eebd831a6c78e462575c
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-10-17 18:28:17 -0300

    requisito 1

--- src/models/conection.js
+++ src/models/conection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
